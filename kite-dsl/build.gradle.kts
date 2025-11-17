61cf9f4bf3bfa81febe6f4a4f831d67eca67bfdb
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2025-11-17 10:31:52 +0000

    build(dsl): Add kotlin-script-runtime for better IDE support
    Added kotlin-script-runtime dependency to improve IntelliJ IDEA support
    for @DependsOn and @Repository annotations in .kite.kts files.
    
    This should help with script runtime resolution in the IDE.

--- kite-dsl/build.gradle.kts
+++ kite-dsl/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host")
     implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies")
     implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies-maven")
+    implementation("org.jetbrains.kotlin:kotlin-script-runtime")
 
     // IDE Support: These dependencies are needed for IntelliJ to properly support
     // @DependsOn and @Repository annotations in .kite.kts files
