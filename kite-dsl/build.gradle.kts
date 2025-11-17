f8f99483e0a5d9b4d70b75ab1e52601ca32d102d
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2025-11-17 11:37:33 +0000

    Revert "fix(dsl): Change Guice dependencies back to 'implementation' for IDE support"
    This reverts commit a9750f6972608c6b11f774f0ab5ad22d4dd07d4a.

--- kite-dsl/build.gradle.kts
+++ kite-dsl/build.gradle.kts
@@ -11,10 +11,11 @@ dependencies {
 
     // IDE Support: These dependencies are needed for IntelliJ to properly support
     // @DependsOn and @Repository annotations in .kite.kts files
-    // They must be 'implementation' (not compileOnly) so they're available for script loading
-    implementation("com.google.inject:guice:4.2.2")
-    implementation("org.eclipse.sisu:org.eclipse.sisu.inject:0.3.5")
-    implementation("javax.inject:javax.inject:1")
+    // They are already included transitively at runtime via kotlin-scripting-dependencies-maven
+    // but need to be explicitly declared for IDE script definition loading
+    compileOnly("com.google.inject:guice:4.2.2")
+    compileOnly("org.eclipse.sisu:org.eclipse.sisu.inject:0.3.5")
+    compileOnly("javax.inject:javax.inject:1")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
